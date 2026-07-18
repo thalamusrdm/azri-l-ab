c5250b8aeb68a79c9e181b04f66ae32af89c2f55
thalamusrdm <dor@thalamusrdm.com>
2026-07-18 18:45:14 +0300

    Add app/products/i-see-all/page.tsx

---
+++ app/products/i-see-all/page.tsx
@@ -0,0 +1,3 @@
+import ISee,{metadata} from "../i-see/page";
+export {metadata};
+export default function ISeeAll(){return <><div className="launchbar"><div><span>i See All · LAUNCH OFFER</span><b><s>$80</s> $10</b></div><button>BUY & DOWNLOAD →</button></div><ISee/></>}
