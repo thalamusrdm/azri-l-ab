1dcb9b8ca8825cc95ba0be7557a7657a820c213a
thalamusrdm <dor@thalamusrdm.com>
2026-07-18 19:01:16 +0300

    Make i See All bundle-exclusive

--- app/products/i-see-all/page.tsx
+++ app/products/i-see-all/page.tsx
@@ -1,3 +1,3 @@
 import ISee,{metadata} from "../i-see/page";
 export {metadata};
-export default function ISeeAll(){return <><div className="launchbar"><div><span>i See All · LAUNCH OFFER</span><b><s>$80</s> $10</b></div><button>BUY & DOWNLOAD →</button></div><ISee/></>}
+export default function ISeeAll(){return <><div className="launchbar exclusive"><div><span>i See All · BUNDLE EXCLUSIVE</span><b><s>$400</s> $60</b></div><a href="/products/i-see-complete">GET THE COMPLETE BUNDLE →</a></div><ISee/></>}
